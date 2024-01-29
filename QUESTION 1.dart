db570a768c5f6d525b1f76648e94c00db516318a
ArEeb QurEshi <areebulameenq12345@gmail.com>
2024-01-29 16:17:52 +0500

    Question 1 completed

---
+++ QUESTION 1.dart
@@ -0,0 +1,9 @@
+// << QUESTION NO: 1 >>
+//  Q.1: Create a list of names and print all names using list.
+
+// << Answer NO: 1 >>
+
+void main(){
+  List<String> names =["ArEeb", "Ali", "Usman", "Basit", "Zain", "Kaif"];
+  print(names);
+}
